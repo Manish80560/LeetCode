86a293de7d2aec671529bc504d3f92559604b3f8
Manish <mskasaudhan12@gmail.com>
2026-01-16 22:16:02 +0530

    Time: 541 ms (89.57%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
 
-DELETE p1
-FROM Person p1
-JOIN Person p2
-ON p1.email = p2.email
-AND p1.id > p2.id;
+-- DELETE p1
+-- FROM Person p1
+-- JOIN Person p2
+-- ON p1.email = p2.email
+-- AND p1.id > p2.id;
+
+delete from person where id not in(
+select id from( select id, email, Row_number() over(partition by email order by id ) as rn
+from Person ) as p where rn=1);
