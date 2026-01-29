50c9b5491bb41e17b90116ea3b1123649f6deb57
Manish <mskasaudhan12@gmail.com>
2026-01-29 22:11:12 +0530

    Time: 555 ms (27.88%) | Memory: 0B (100.00%) - LeetSync

--- 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -1,19 +1,37 @@
 # Write your MySQL query statement below
-SELECT lb.book_id,
-       lb.title,
-       lb.author,
-       lb.genre,
-       lb.publication_year,
-       lb.total_copies AS current_borrowers
-FROM library_books lb
-INNER JOIN borrowing_records br
-    ON br.book_id = lb.book_id
-WHERE br.return_date IS NULL
-GROUP BY lb.book_id,
-         lb.title,
-         lb.author,
-         lb.genre,
-         lb.publication_year,
-         lb.total_copies
-HAVING COUNT(*) = lb.total_copies
-ORDER BY current_borrowers DESC, lb.title;
+-- SELECT lb.book_id,
+--        lb.title,
+--        lb.author,
+--        lb.genre,
+--        lb.publication_year,
+--        lb.total_copies AS current_borrowers
+-- FROM library_books lb
+-- INNER JOIN borrowing_records br
+--     ON br.book_id = lb.book_id
+-- WHERE br.return_date IS NULL
+-- GROUP BY lb.book_id,
+--          lb.title,
+--          lb.author,
+--          lb.genre,
+--          lb.publication_year,
+--          lb.total_copies
+-- HAVING COUNT(*) = lb.total_copies
+-- ORDER BY current_borrowers DESC, lb.title;
+
+
+SELECT 
+    l.book_id,
+    l.title,
+    l.author,
+    l.genre,
+    l.publication_year,
+    l.total_copies AS current_borrowers
+FROM library_books l
+WHERE l.total_copies = (
+    SELECT COUNT(*)
+    FROM borrowing_records b
+    WHERE b.book_id = l.book_id
+      AND b.return_date IS NULL
+)
+ORDER BY current_borrowers DESC,
+l.title asc;
