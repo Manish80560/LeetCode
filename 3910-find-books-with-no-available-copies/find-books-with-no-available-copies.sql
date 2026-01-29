3a463623043cfe9efe64033cb3dca9b4afb3d279
Manish <mskasaudhan12@gmail.com>
2026-01-29 22:10:35 +0530

    Time: 675 ms (10.44%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+SELECT lb.book_id,
+       lb.title,
+       lb.author,
+       lb.genre,
+       lb.publication_year,
+       lb.total_copies AS current_borrowers
+FROM library_books lb
+INNER JOIN borrowing_records br
+    ON br.book_id = lb.book_id
+WHERE br.return_date IS NULL
+GROUP BY lb.book_id,
+         lb.title,
+         lb.author,
+         lb.genre,
+         lb.publication_year,
+         lb.total_copies
+HAVING COUNT(*) = lb.total_copies
+ORDER BY current_borrowers DESC, lb.title;
