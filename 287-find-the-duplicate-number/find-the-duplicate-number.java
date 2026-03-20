8a1620aa03957ca2e49d260a1476c9a96671c4ac
Manish <mskasaudhan12@gmail.com>
2026-03-20 16:19:39 +0530

    Time: 22 ms (28.29%) | Memory: 92.1 MB (10.48%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,49 +1,49 @@
-// class Solution {
-//     public int findDuplicate(int[] nums) {
-
-//         // for O(n^2) time complexity
-//         // for(int i = 0 ; i < nums.length ; i++){
-//         //     for(int j = i + 1 ; j < nums.length ; j++){
-//         //         if(nums[i] == nums[j]){
-//         //             return nums[i];
-//         //         }
-//         //     }
-//         // }
-//         // return -1;
-
-
-
-//         Set<Integer> set = new HashSet<>();
-
-//         for(int num : nums){
-//             if(set.contains(num)){
-//                 return num;
-//             }
-//             set.add(num);
-
-//         }
-//         return -1;
-        
-//     }
-// }
-
-
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> st = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(st.contains(nums[i])) return nums[i];
-            else st.add(nums[i]);
+
+        // for O(n^2) time complexity
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     for(int j = i + 1 ; j < nums.length ; j++){
+        //         if(nums[i] == nums[j]){
+        //             return nums[i];
+        //         }
+        //     }
+        // }
+        // return -1;
+
+
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            if(set.contains(num)){
+                return num;
+            }
+            set.add(num);
+
         }
         return -1;
-    }
-         static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
-                writer.write("0");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }));
+        
     }
 }
+
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         Set<Integer> st = new HashSet<>();
+//         for(int i=0;i<nums.length;i++){
+//             if(st.contains(nums[i])) return nums[i];
+//             else st.add(nums[i]);
+//         }
+//         return -1;
+//     }
+//          static {
+//         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//             try (FileWriter writer = new FileWriter("display_runtime.txt")) {
+//                 writer.write("0");
+//             } catch (IOException e) {
+//                 e.printStackTrace();
+//             }
+//         }));
+//     }
+// }
