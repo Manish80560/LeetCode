0049d880a14b5a388ccebb0db628238f3ffae0b1
Manish <mskasaudhan12@gmail.com>
2026-03-25 20:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (7.81%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,18 +3,26 @@ public:
     bool isPalindrome(int x) {
 
         int org = x;
-        int rem ;
-        long long ans = 0;
-        if(x < 0){
-            return false;
-        }
+        
+        // int rem ;
+        // long long ans = 0;
+        // if(x < 0){
+        //     return false;
+        // }
 
-        while(org > 0){
-            rem = org % 10;
-            ans = ans * 10 + rem;
-            org /= 10;  
-        }
-        return (ans == x);
+        // while(org > 0){
+        //     rem = org % 10;
+        //     ans = ans * 10 + rem;
+        //     org /= 10;  
+        // }
+        // return (ans == x);
+
+        string s = to_string(x);   // convert int → string
+        string rev = s;
+
+        reverse(rev.begin(), rev.end());  // reverse string
+
+        return s == rev;
         
     }
 };
