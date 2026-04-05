fae1f4b1147d9e5f6d49cb07eceecbba805c5079
Manish <mskasaudhan12@gmail.com>
2026-04-05 16:58:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (25.60%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.c
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.c
@@ -6,20 +6,20 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    if(list1 == NULL){
-        return list2;
-    }
-    if(list2 == NULL){
-        return list1;
-    }
-
-    // if(list1 == NULL || list2 == NULL){
-    //     if(list1 == NULL){
-    //         return list2;
-    //     }else{
-    //         return list1;
-    //     }
+    // if(list1 == NULL){
+    //     return list2;
     // }
+    // if(list2 == NULL){
+    //     return list1;
+    // }
+
+    if(list1 == NULL || list2 == NULL){
+        if(list1 == NULL){
+            return list2;
+        }else{
+            return list1;
+        }
+    }
 
 	if(list1 -> val <= list2 -> val){
 		list1 -> next = mergeTwoLists(list1 -> next, list2);
