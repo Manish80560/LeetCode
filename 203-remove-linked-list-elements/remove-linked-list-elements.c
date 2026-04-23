5d20c3390b43dad7ac682337217be950d34fe8ef
Manish <mskasaudhan12@gmail.com>
2026-04-23 15:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (43.89%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.c
+++ 203-remove-linked-list-elements/remove-linked-list-elements.c
@@ -6,21 +6,16 @@
  * };
  */
 struct ListNode* removeElements(struct ListNode* head, int val) {
-           struct  ListNode* temp = head;
+        struct  ListNode* temp = head;
 
-        // handle deletion at head
         while (temp != NULL && temp->val == val) {
             temp = temp->next;
         }
 
         head = temp;
 
-        // main logic (same idea as your code)
         while (temp != NULL && temp->next != NULL) {
             
-            // cout fixed (optional debug)
-            // cout << temp->val << " ";
-
             if (temp->next->val == val) {
                 temp->next = temp->next->next;
             } else {
