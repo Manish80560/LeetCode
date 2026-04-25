d9ff3c019f867162e79e13b39b8f699e27ba3774
Manish <mskasaudhan12@gmail.com>
2026-04-25 21:42:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (37.28%) - LeetSync

--- 292-nim-game/nim-game.java
+++ 292-nim-game/nim-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canWinNim(int n) {
-          if(n%4 == 0){
-    return false;
-   }else{
-    return true;
-   }
+        if(n % 4 != 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
