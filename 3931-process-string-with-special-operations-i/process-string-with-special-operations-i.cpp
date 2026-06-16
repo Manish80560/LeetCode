45cc72ab2c6d588dbf0762a241efb452284c0e20
Manish <mskasaudhan12@gmail.com>
2026-06-16 16:32:05 +0530

    Time: 4 ms (63.64%) | Memory: 39.9 MB (42.05%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -2,39 +2,41 @@ class Solution {
 public:
     string processStr(string s) {
 
-        // string res;
+        string res;
 
-        // for(char c : s){
-        //     if(c >= 'a' && c <= 'z'){
-        //         res.push_back(c);
-        //     }else if(c == '*'){
-        //         if(!res.empty())
-        //         res.pop_back();
-        //     }else if(c == '#'){
-        //         res += res;
-        //     }else if(c == '%'){
-        //         reverse(res.begin() , res.end());
-        //     }
-        // }
-        // return res;
-
-
-        int n = s.size();
-        string ans = "";
         for(char c : s){
-         if(c >= 'a' && c <= 'z'){
-            ans.push_back(c);
-         }
-
-         else if(c == '*'){
-            if(!ans.empty()) ans.pop_back();
-         }
-         else if(c=='#'){
-            ans+=ans;
-         }else if(c == '%'){
-            reverse(ans.begin(),ans.end());
-         }
+            if(c >= 'a' && c <= 'z'){
+                res.push_back(c);
+            }else if(c == '*'){
+                if(!res.empty())
+                res.pop_back();
+            }else if(c == '#'){
+                res += res;
+            }else if(c == '%'){
+                reverse(res.begin() , res.end());
+            }
         }
-        return ans;
+        return res;
+
+
+// for less time
+
+        // int n = s.size();
+        // string ans = "";
+        // for(char c : s){
+        //  if(c >= 'a' && c <= 'z'){
+        //     ans.push_back(c);
+        //  }
+
+        //  else if(c == '*'){
+        //     if(!ans.empty()) ans.pop_back();
+        //  }
+        //  else if(c=='#'){
+        //     ans+=ans;
+        //  }else if(c == '%'){
+        //     reverse(ans.begin(),ans.end());
+        //  }
+        // }
+        // return ans;
     }
 };
