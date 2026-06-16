233a50070a0c2b378c16664e3afa4139b407a98f
Manish <mskasaudhan12@gmail.com>
2026-06-16 16:31:08 +0530

    Time: 6 ms (39.02%) | Memory: 39.6 MB (87.50%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -2,20 +2,39 @@ class Solution {
 public:
     string processStr(string s) {
 
-        string res;
+        // string res;
 
+        // for(char c : s){
+        //     if(c >= 'a' && c <= 'z'){
+        //         res.push_back(c);
+        //     }else if(c == '*'){
+        //         if(!res.empty())
+        //         res.pop_back();
+        //     }else if(c == '#'){
+        //         res += res;
+        //     }else if(c == '%'){
+        //         reverse(res.begin() , res.end());
+        //     }
+        // }
+        // return res;
+
+
+        int n = s.size();
+        string ans = "";
         for(char c : s){
-            if(c >= 'a' && c <= 'z'){
-                res.push_back(c);
-            }else if(c == '*'){
-                if(!res.empty())
-                res.pop_back();
-            }else if(c == '#'){
-                res += res;
-            }else if(c == '%'){
-                reverse(res.begin() , res.end());
-            }
+         if(c >= 'a' && c <= 'z'){
+            ans.push_back(c);
+         }
+
+         else if(c == '*'){
+            if(!ans.empty()) ans.pop_back();
+         }
+         else if(c=='#'){
+            ans+=ans;
+         }else if(c == '%'){
+            reverse(ans.begin(),ans.end());
+         }
         }
-        return res;
+        return ans;
     }
 };
