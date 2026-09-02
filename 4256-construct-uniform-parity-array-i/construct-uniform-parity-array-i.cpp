fe33d6fffaa6d694c3369faf4823eff92d02ee29
Manish <mskasaudhan12@gmail.com>
2026-09-02 22:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (67.80%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
+        // int n = nums1.size();
+        
+        // vector<int>nums2;
 
-        // if(n <= 1) return true;
-        vector<int>nums2;
-
-        for (int num : nums1){
-            if(num % 2 == 0){
-                nums2.push_back(num - 1);
-            }else{
-                nums2.push_back(num);
-            }
-        }
+        // for (int num : nums1){
+        //     if(num % 2 == 0){
+        //         nums2.push_back(num - 1);
+        //     }else{
+        //         nums2.push_back(num);
+        //     }
+        // }
         
         return true;
     }
