2e60f53014dff11a79919d58a12c1c8c760b9182
Manish <mskasaudhan12@gmail.com>
2026-09-23 17:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.11%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // using builin functions
-       /* double res =  pow(x,n);
-        return res; */
+        // using built-in method
+        // return pow(x, n);
 
-        // // using recursion
-        // if(x == 0){
-        //     return 1;
-        // }else{
-        //     return myPow(x - 1 , n );
-        // }
-        return pow(x,n);
+        long long exp = n;
+
+        if (exp < 0) {
+            x = 1.0 / x;
+            exp = -exp;
+        }
+
+        return power(x, exp);
+    }
+
+    double power(double x, long long n) {
+        // using recursion
+        if (n == 0) {
+            return 1.0;
+        }
+
+        if (n % 2 == 0) {
+            double half = power(x, n / 2);
+            return half * half;
+        } else {
+            return x * power(x, n - 1);
+        }
     }
 };
