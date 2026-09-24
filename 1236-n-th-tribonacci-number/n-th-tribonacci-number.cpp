50a6601ea0b1fef211290dbbe64564a00ed027d7
Manish <mskasaudhan12@gmail.com>
2026-09-25 03:04:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (7.36%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n + 1 , -1);
+
+        return solve(n , dp);
+
+    }
+    
+    int solve(int n , vector<int>& dp){
+
+        if(n <= 1){
+            return n;
+        }
+
+        if(n == 2){
+            return 1;
+        }
+
+        if(dp[n] == -1){
+            dp[n] = solve(n - 1 , dp) + solve(n - 2 , dp) + solve(n - 3 , dp);
+        }
+        return dp[n];
+    }
+};
